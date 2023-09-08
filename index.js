35b8de09fb8d9f3a57e594c76c4a990f03963947
SahilC44 <144429641+SahilC44@users.noreply.github.com>
2023-09-08 13:49:24 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+const passport = require("./middleware/authentication.js");
+const cookieSession = require("cookie-session");
+const stockControler = require("./controller/stockcontroller.js");
+const onclickControler = require("./controller/onclick.controller.js");
+const path = require("path");
+const mypath = path.join(__dirname, "public");
+
+app.use(express.static(mypath));
+app.use(express.json());
+app.use("/stocks", stockControler);
+app.use("/redis_pro", onclickControler);
+
+app.use(
+  cookieSession({
+    name: "tuto-session",
+    keys: ["key1"],
+  })
+);
+
+const Email = require("./controller/emailController.js");
+
+app.use("/email", Email);
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.render("home");
+  }
+);
+
+app.get("/auth/google/failure", (req, res) => {
+  return res.send("Failure");
+});
+
+app.get("/index", (req, res) => {
+  res.render("index");
+});
+
+app.get("/stockPage", (req, res) => {
+  res.render("stockPage");
+});
+app.get("/Home", (req, res) => {
+  res.render("Home");
+});
+app.get("/MutualFunds", (req, res) => {
+  res.render("MutualFunds");
+});
+app.get("/Grow_cart_page", (req, res) => {
+  res.render("Grow_cart_page");
+});
+
+app.get("/logout", (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect("/index");
+});
+
+app.listen(6789,()=>{
+  console.log("✔️");
+})
+
+module.exports = app;
